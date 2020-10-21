72ba2a4862ce27e942431b96214178f36382b3da
Arczi <piotr.borysowski@gmail.com>
2020-10-21 18:41:56 +0200

    small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState }  from "react"
+
+import { navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { createBoard } from '../services/board';
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const [name, setName] = useState('');
+
+  const onCreateBoard = () => {
+    createBoard()
+      .then((id) => navigate('/board/'+id));
+  }
+
+  return ( <Layout>
     <SEO title="Home" />
-    <Link to="/board/">Create new board</Link>
-  </Layout>
-)
+    {name}
+    <input placeholder="Name your gift's hat" value={name} onChange={(e) => setName(e.target.value)} />
+    <button disabled={name === ''} onClick={onCreateBoard}>Create your hat</button>
+  </Layout>);
+}
 
 export default IndexPage
--- src/services/board.js
+++ src/services/board.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 const url = process.env.NODE_ENV === 'production' ? process.env.GATSBY_API_URL : '';
 
 export async function createBoard() {
-  // return axios.get(url + '/api/createBoard')
-  //   .then(response => {
-  //     return response.data;
-  //   });
-  console.log(newBoardRef);
+  return axios.get(url + '/api/createBoard')
+    .then(response => {
+      return response.data;
+    });
+  // console.log(newBoardRef);
 }
